5111895f64b46b81baedd73a3a101808fb0e0080
Suryansh Shukla <suryanshshukla011@gmail.com>
2024-10-02 18:32:43 +0530

    updating readme and codes

---
+++ Java/first.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class first
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        System.out.println("The number you entered is : "+n);
+        sc.close();
+    }
+}
